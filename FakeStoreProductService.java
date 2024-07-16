57991f125aed03f96e9aeddfc82d3f83a43fe627
lilli2203 <mailyourlilli@gmail.com>
2024-07-17 01:30:49 +0530

    up

--- FakeStoreProductService.java
+++ FakeStoreProductService.java
@@ -1,4 +1,5 @@
 package dev.naga.productservice_sst.services;
+
 import dev.naga.productservice_sst.dtos.FakeStoreProductDto;
 import dev.naga.productservice_sst.models.Product;
 import org.springframework.stereotype.Service;
@@ -8,40 +9,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class FakeStoreProductService implements  ProductService {
-    @Override
+public class FakeStoreProductService implements ProductService {
 
+    @Override
     public Product getProductById(Long id) {
-//        throw new RuntimeException("Not implemented");
         RestTemplate restTemplate = new RestTemplate();
         FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject("http://fakestoreapi.com/products/" + id, FakeStoreProductDto.class);
         return convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
-
     }
-    public Product convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto){
+
+    public List<Product> getAllProducts() {
+        RestTemplate restTemplate = new RestTemplate();
+        FakeStoreProductDto[] fakeStoreProductDtos = restTemplate.getForObject("http://fakestoreapi.com/products", FakeStoreProductDto[].class);
+        List<Product> products = new ArrayList<>();
+        for (FakeStoreProductDto fakeStoreProductDto : fakeStoreProductDtos) {
+            products.add(convertFakeStoreProductDtoToProduct(fakeStoreProductDto));
+        }
+        return products;
+    }
+
+    public Product convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto) {
         Product product = new Product();
         product.setId(fakeStoreProductDto.getId());
         product.setTitle(fakeStoreProductDto.getTitle());
         product.setDescription(fakeStoreProductDto.getDescription());
         product.setPrice(fakeStoreProductDto.getPrice());
         product.setImage(fakeStoreProductDto.getImage());
+        product.setRating(fakeStoreProductDto.getRating());
+        product.setCategory(fakeStoreProductDto.getCategory());
+        product.setStock(fakeStoreProductDto.getStock());
+        product.setBrand(fakeStoreProductDto.getBrand());
+        product.setDimensions(fakeStoreProductDto.getDimensions());
+        product.setWeight(fakeStoreProductDto.getWeight());
+        product.setSupplier(fakeStoreProductDto.getSupplier());
         return product;
     }
-    public List<Product> getAllProducts(){
+
+    public List<Product> getAllProducts(int page, int size) {
         RestTemplate restTemplate = new RestTemplate();
-        FakeStoreProductDto[] fakeStoreProductDtos = restTemplate.getForObject("http://fakestoreapi.com/products",FakeStoreProductDto[].class);
+        String url = String.format("http://fakestoreapi.com/products?_page=%d&_limit=%d", page, size);
+        FakeStoreProductDto[] fakeStoreProductDtos = restTemplate.getForObject(url, FakeStoreProductDto[].class);
         List<Product> products = new ArrayList<>();
-        for(FakeStoreProductDto fakeStoreProductDto: fakeStoreProductDtos){
+        for (FakeStoreProductDto fakeStoreProductDto : fakeStoreProductDtos) {
             products.add(convertFakeStoreProductDtoToProduct(fakeStoreProductDto));
         }
-
         return products;
-
     }
 
+    public Product getProductByIdWithErrorHandling(Long id) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject("http://fakestoreapi.com/products/" + id, FakeStoreProductDto.class);
+            return convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to retrieve product with id: " + id, e);
+        }
+    }
 
+    public Product getProductByIdAsync(Long id) {
+        RestTemplate restTemplate = new RestTemplate();
+        FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject("http://fakestoreapi.com/products/" + id, FakeStoreProductDto.class);
+        return convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
+    }
 }
-
-
-
-
