ac04d6638777ce029ad82af4b49967fecbc4b7ef
lilli2203 <mailyourlilli@gmail.com>
2024-07-17 01:14:38 +0530

    Add files via upload

---
+++ ExceptionDto.java
@@ -0,0 +1,11 @@
+package dev.naga.productservice_sst.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionDto {
+    private String message;
+    private String resolution;
+}
