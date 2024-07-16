47e4b905227d38e7e2431d535ce14f13f03c8898
lilli2203 <mailyourlilli@gmail.com>
2024-07-17 01:24:46 +0530

    added models

---
+++ Category.java
@@ -0,0 +1,13 @@
+package dev.naga.productservice_sst.models;
+
+import java.security.PrivateKey;
+
+public class Category {
+    private Long id;
+    private String title;
+    private String description;
+
+
+
+
+}
